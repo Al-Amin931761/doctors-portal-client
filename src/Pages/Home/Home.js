d4da2a4ee4a05260fc0b77c1b20103465b509094
Al-Amin931761 <alamin931761@gmail.com>
2022-05-13 12:01:42 +0600

    calendar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,10 +6,11 @@ import Info from './Info';
 import MakeAppointment from './MakeAppointment';
 import Services from './Services';
 import Testimonials from './Testimonials';
+import Footer from '../Shared/Footer';
 
 const Home = () => {
     return (
-        <div className='px-12'>
+        <div>
             <Banner></Banner>
             <Info></Info>
             <Services></Services>
@@ -17,6 +18,7 @@ const Home = () => {
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
             <ContactUs></ContactUs>
+            <Footer></Footer>
         </div>
     );
 };
