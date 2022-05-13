e5e452e0a460a5a5c20438f748cebd36a318942d
Al-Amin931761 <alamin931761@gmail.com>
2022-05-13 13:26:54 +0600

    conditional rendering

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
+import Service from './Service';
 
 
 const AvailableAppointments = ({ date }) => {
@@ -14,6 +15,12 @@ const AvailableAppointments = ({ date }) => {
     return (
         <div>
             <h4 className='text-xl text-secondary text-center'>Available Appointments on {format(date, 'PP')}</h4>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service key={service._id} service={service}></Service>)
+                }
+            </div>
         </div>
     );
 };
